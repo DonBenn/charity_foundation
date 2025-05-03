a491d3c67e46c45e07765b2640ca3ed7e9ca4295
Denis Bessonov <denibessono@yandex.ru>
2025-05-03 18:26:18 +0300

    Create Donation model

---
+++ app/models/donation.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from sqlalchemy import Column, String, Text, DateTime, Integer, Boolean, \
+    ForeignKey
+
+from app.core.db import Base
+
+
+class Donation(Base):
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    comment = Column(Text, nullable=False)
+    full_amount = Column(Integer, nullable=False)
+    invested_amount = Column(Integer, default=0)
+    fully_invested = Column(Boolean, default=False)
+    create_date = Column(DateTime, default=datetime.now)
+    close_date = Column(DateTime)
